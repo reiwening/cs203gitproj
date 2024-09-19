9e5069ef22a6b0cd0e37d91aeb14c4e391c28acb
Reiwen <reiwen.ing.2023@scis.smu.edu.sg>
2024-09-19 18:34:44 +0800

    The very first commit of cs203 project

---
+++ src/main/java/com/g5/Knight2MeetYou/Knight2MeetYouApplication.java
@@ -0,0 +1,13 @@
+package com.g5.Knight2MeetYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knight2MeetYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knight2MeetYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g5/Knight2MeetYou/Knight2MeetYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.Knight2MeetYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knight2MeetYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
